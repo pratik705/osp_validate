3b5dba54a0f714944bc5bc59aa0ec73d3ee3fd39
Pratik Bandarkar <pratik.bandarkar@gmail.com>
2020-12-04 18:50:27 +0000

    Fix output

--- output.tf
+++ output.tf
@@ -1,4 +1,3 @@
-/*
 output "instance_id" {
   value = module.nova.instance_id
 }
@@ -10,4 +9,3 @@ output "instance_floating_ip" {
 output "instance_ssh_user" {
   value = module.nova.instance_ssh_user
 }
-*/
