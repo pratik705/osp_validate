6dce6001475eebf1dba65cb8e0ab57d1e0375068
Pratik Bandarkar <pratik.bandarkar@gmail.com>
2020-12-21 18:40:00 +0000

    Fix subnet dependency

--- neutron/subnet.tf
+++ neutron/subnet.tf
@@ -1,4 +1,5 @@
 resource "openstack_networking_subnet_v2" "internal_subnet" {
   network_id = openstack_networking_network_v2.internal_network.id
   cidr       = var.network_cidr
+  depends_on = [openstack_networking_network_v2.internal_network]
 }
