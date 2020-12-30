a17f8c645574c15a3aeab705712e1417fe509fb1
rackspace <rhosp@rackspace.com>
2020-12-30 02:00:38 +0000

    Decouple file system creation

--- nova/instance.tf
+++ nova/instance.tf
@@ -73,22 +73,25 @@ resource "openstack_compute_floatingip_associate_v2" "fip_volume" {
 
 resource "openstack_compute_volume_attach_v2" "attach_volume" {
   instance_id = openstack_compute_instance_v2.instance_i1_image[count.index].id
-  volume_id   = module.cinder.volume_id[count.index]
-  count       = var.boot_from_volume ? 0 : var.instance_count
-  provisioner "remote-exec" {
+  volume_id = module.cinder.volume_id[count.index]
+  count = var.boot_from_volume ? 0 : var.instance_count
+}
+
+resource "null_resource" "create_fs" {
+    connection {
+      timeout     = "2m"
+      host        = openstack_networking_floatingip_v2.fip_1[count.index].address
+      user        = var.image_user_name
+      private_key = var.ssh_private_key != null ? file(var.ssh_private_key) : null
+    }
+    provisioner "remote-exec" {
     on_failure = continue
     inline = [
       "sudo mkfs.xfs /dev/vdb",
       "sudo mount /dev/vdb /mnt",
-      "sudo touch /mnt/test_file.txt"
+      "echo 'This is a test file' | sudo  tee /mnt/test_file.txt"
     ]
-
-//    connection {
-//      timeout     = "2m"
-//      host        = openstack_networking_floatingip_v2.fip_1[count.index].address
-//      user        = var.image_user_name
-//      private_key = var.ssh_private_key != null ? file(var.ssh_private_key) : null
-//    }
   }
+  count = var.boot_from_volume ? 0 : (var.external_network != null ? 1 : 0 )
+  depends_on = [openstack_compute_volume_attach_v2.attach_volume]
 }
-
